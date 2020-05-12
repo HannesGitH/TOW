d3288a890f7933e47e7eb6d99d305288fe8b3f88
HannesGitH <hanneshattenbach.dev@gmail.com>
2020-05-12 19:47:05 +0200

    choose team

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,11 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+Color team1_color = Color.fromARGB(255, 255, 56, 56);
+Color team2_color = Color.fromARGB(255, 62, 238, 238);
 
 void main() => runApp(MyApp());
 
@@ -7,35 +14,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      title: 'TOW Worldwide',
+      darkTheme: ThemeData(
+        primaryColor: team1_color,
+        accentColor: team1_color,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      theme: ThemeData(
+        primaryColor: team2_color,
+        accentColor: team2_color,
+      ),
+      home: MyHomePage(title: 'TOW : TugOfWar Worldwide'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -44,68 +38,93 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  ///0: none; 1:blue; 2:red
+  Future<int> getTeam() async {
+    //return 0;//only to debug
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return (await prefs.getInt('team'))??0;
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    return FutureBuilder(
+      future: getTeam(),
+      initialData: 0,
+      builder: (context, snap) {
+        if (snap.connectionState != ConnectionState.done) {
+          return Center(child: CircularProgressIndicator());
+        }
+        if (snap.data == 0) {
+          return TeamChoose();
+        }
+        return Scaffold(
+          appBar: AppBar(
+            title: Text(widget.title),
+          ),
+          body: Text("you shouldn't be here"),
+        );
+      },
+    );
+  }
+}
+
+class TeamChoose extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Choose your team'),
+        backgroundColor: Colors.white,
+        centerTitle: true,
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
+            TeamButton(team: 1),
+            TeamButton(team: 2),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class TeamButton extends StatelessWidget {
+  int team;
+  TeamButton({this.team = 1});
+
+  _setTeam(int t) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setInt('team', t);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: EdgeInsets.all(40),
+        child: RaisedButton(
+          elevation: 14,
+          shape: RoundedRectangleBorder(borderRadius:BorderRadius.circular(30),),
+          color: team == 1 ? team1_color : team2_color,
+          onPressed: () {
+            _setTeam(team);
+            runApp(MyApp());
+          },
+          child:  Padding(
+            padding: EdgeInsets.all(40),
+            child: Transform(
+              transform: team==1?Matrix4.identity():(Matrix4.identity()..rotateY(pi)..translate(-200.0,0,0)),
+              child: SvgPicture.asset(
+                'assets/tugger.svg',
+                color: Colors.black54,
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
